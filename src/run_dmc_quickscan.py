729a002240608f69144905dfde9ad01c1f2879a5
ncail <n3var1as@gmail.com>
2026-04-10 17:07:37 -0700

    successful test of combining root files for scan

--- src/run_dmc_quickscan.py
+++ src/run_dmc_quickscan.py
@@ -42,32 +42,44 @@ def main():
     Entry point for DMC QuickScan execution.
 
     Configuration:
-        file_pattern : Directory or file pattern to your simulation output ROOT files
-        output_dir   : Directory where you want TES trace plots to be output
-        file_mode    : 
-        combined_name:
-        detector     :
-        flip         :
-        normalize    :
-        xlim         :
+        file_pattern (str) : Directory or file pattern to your simulation output ROOT files
+        output_dir   (str) : Directory where you want TES trace plots to be output
+        file_mode    (str) : Mode for handling input files ("single" or "combined")
+            - "single"     : Process a single ROOT file
+            - "combined"   : Combine multiple ROOT files into one for a simulation-wide scan
+        combined_name (str): Name for the combined ROOT file if file_mode is "combined"
+        detector     (int) : Detector ID to scan (1-6 for a SuperCDMS Tower)
+        flip         (bool): Whether to flip the TES trace plots
+        normalize    (bool): Whether to normalize the TES trace plots
+        xlim        (tuple): X-axis limits for the TES trace plots in microseconds (e.g., (25, 50) to focus on the main pulse region)
     """
     config = ScanConfig(
-        file_pattern="/home/nevenac/scratch/CUTE-T3_Ba133_12inch_DMC_10kevents/*.root",
-        output_dir="/home/nevenac/projects/scdms-dmc/output/",
+        file_pattern="/home/nevenac/scratch/CUTE-T3_Ba133_12inch_DMC_10kevents/CUTE*.root",
+        output_dir="/home/nevenac/projects/scdms-dmc/output/testing_scanner",
         file_mode="combined",
         combined_name="run_combined.root",
-        detector=3,
+        detector=2,
         flip=True,
         normalize=False,
         xlim=(25, 50),
     )
 
+    # Load ROOT files, optionally combine into one, and initialize the DMCQuickScan object
     scanner = DMCQuickScan(config)
 
     # Print to terminal summary of each DMC ROOT branch
     scanner.print_summary()
+
+    # Stores the EventNum indices that registered per detector
     scanner.build_index()
-    scanner.run_sanity_plots()
+
+    # Generate TES trace sanity plots for all events in the selected detector (configured above)
+    # Each plot is all 12 channel traces for an event. 
+    # Arguments --
+    #   None: All events that occurred in the detector are looped through and plotted.
+    #   int: The first N events that occurred in the detector are plotted (e.g., 10 to plot the first 10 events).
+    #   Tuple[int, int]: A range of events to plot (e.g., (0, 10) to plot the first 10 events)
+    scanner.run_sanity_plots(3)
 
 
 if __name__ == "__main__":
